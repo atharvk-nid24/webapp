3994fda1ae129673c489799b537dd642c4fe3105
atharvk-nid24 <atharv.k@nid.edu>
2026-01-13 12:50:37 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Firebase SDKs loaded via CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+window.db = getFirestore(app);
